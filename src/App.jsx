c2b113dea0d43855967aa72d292cfea80fb2f7e1
Jeisson Gómez <jeissongomez@hotmail.es>
2022-05-14 18:10:01 -0500

    Version desktop terminada :desktop_computer:

--- src/App.jsx
+++ src/App.jsx
@@ -8,21 +8,28 @@ function App() {
           <div className="page__logo">
             <img src="../images/logo.svg" alt="logo" />
           </div>
-          <div className="page__image">
-            <img src="../images/illustration-mockups.svg" alt="illustration" />
-          </div>
-          <h2 className="page__title">
-            Build The Community Your Fans Will Love
-          </h2>
-          <p className="page__text">
-            Huddle re-imagines the way we build communities. You have a voice,
-            but so does your audience. Create connections with your users as you
-            engage in genuine discussion.
-          </p>
-          <div className="page__btn">
-            <a className="page__btn-register" href="#">
-              Register
-            </a>
+          <div className="main">
+            <div className="page__image">
+              <img
+                src="../images/illustration-mockups.svg"
+                alt="illustration"
+              />
+            </div>
+            <div className="page__description">
+              <h2 className="page__title">
+                Build The Community Your Fans Will Love
+              </h2>
+              <p className="page__text">
+                Huddle re-imagines the way we build communities. You have a
+                voice, but so does your audience. Create connections with your
+                users as you engage in genuine discussion.
+              </p>
+              <div className="page__btn">
+                <a className="page__btn-register" href="#">
+                  Register
+                </a>
+              </div>
+            </div>
           </div>
           <ul className="page__social">
             <li>
